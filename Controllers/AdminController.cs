c71c7b97a6b8c20721448bc3dfd210282eafc497
SamAndrs <sam_andrs@yahoo.se>
2025-01-28 11:18:43 +0100

    admin account edit done

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -153,8 +153,123 @@ namespace FribergRentalCars.Controllers
             }
         }
 
+        // GET: AdminController/EditAccount/5
+        [AdminAuthorizationFilter]
+        public async Task<ActionResult> EditAccount(int userId)
+        {
+            if(userId == null)
+            {
+                return NotFound();
+            }
 
-        // GET: AdminController/ Editccount/ 5
+            var user = await _userRepo.GetByIdAsync(userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _accRepo.GetIdByAsync(user.UserId);
+            if(account == null)
+            {
+                return NotFound();
+            }
+
+            var adress = await _adrRepo.GetIdByAsync(account.AccountId);
+            if(adress == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EditAccountViewModel
+            {
+                UserId = user.UserId,
+                AccountId = user.AccountId,
+
+                AdressId = account.AdressId,
+                Street = adress.Street,
+                PostalCode = adress.PostalCode,
+                City = adress.City,
+
+                FirstName = account.FirstName,
+                LastName = account.LastName,
+                PhoneNumber = account.PhoneNumber,
+                Email = account.Email,
+
+                UserName = user.UserName,
+                Password = user.Password,
+                IsAdmin = user.IsAdmin
+
+            };
+            return View(model);
+        }
+
+        // POST: AdminController/EditAccount/Model
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAccount(int userId, EditAccountViewModel model)
+        {
+            if(model == null)
+            {
+                ModelState.AddModelError("", "Konto-objektet finns inte.");
+            }
+
+            var user = await _userRepo.GetByIdAsync(userId);
+            var account = await _accRepo.GetIdByAsync(user.UserId);
+            var adress = await _adrRepo.GetIdByAsync(account.AccountId);
+
+            var oldUserName = user.UserName;
+            if(ModelState.IsValid)
+            {
+                if(model.UserName != oldUserName)
+                {
+                    if(await _userRepo.UserNameAvailaibility(model.UserName))
+                    {
+                        ModelState.AddModelError("", "Användarnamnet är redan taget!");
+                    }
+                    else
+                    {
+                        user.UserName = model.UserName;
+                    }
+                }
+                else
+                {
+                    // set User data
+                    user.UserName = model.UserName;
+                    user.Password = model.Password;
+                    user.ConfirmPassword = model.Password;
+
+                    // Set Account data
+                    account.FirstName = model.FirstName;
+                    account.LastName = model.LastName;
+                    account.PhoneNumber = model.PhoneNumber;
+                    account.Email = model.Email;
+
+                    // Set Adress data
+                    adress.Street = model.Street;
+                    adress.PostalCode = model.PostalCode;
+                    adress.City = model.City;
+                }
+                try
+                {
+                    await _userRepo.UpdateAsync(user);
+                    await _accRepo.UpdateAsync(account);
+                    await _adrRepo.UpdateAsync(adress);
+                    return RedirectToAction(nameof(ListAllAccounts));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Det blev något fel");
+                } 
+            }
+
+            //return RedirectToAction(nameof(ListAllAccounts));
+
+            return View(model);
+        }
+
+        #region OLD_EDIT 
+        // GET: AdminController/ EditUser/5
+        /*
         [AdminAuthorizationFilter]
         public async Task<ActionResult> EditUser(int userId)
         {
@@ -172,7 +287,8 @@ namespace FribergRentalCars.Controllers
             return View(user);
         }
 
-        // POST: AdminController/
+        
+        // POST: AdminController/EditUser/5, user
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> EditUser(int id, User user)
@@ -207,6 +323,59 @@ namespace FribergRentalCars.Controllers
             return View(user);
         }
 
+
+        // GET: AdminController/ EditAccount/5
+        [AdminAuthorizationFilter]
+        public async Task<ActionResult> EditAccount(int accountId)
+        {
+            if (accountId == null)
+            {
+                return NotFound();
+            }
+            //var account = await _accRepo.GetIdByAsync(accountId);
+            var account = await _accRepo.GetWithAdressAsync(accountId);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: AdminController/EditAccount/5, account
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAccount(int id, Account account)
+        {
+            if (id != account.AccountId)
+            {
+                NotFound();
+            }
+            //var adress = await _adrRepo.GetIdByAsync(account.AdressId);
+            //account.Adress.Street = adress.Street;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _accRepo.UpdateAsync(account);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (account.AccountId == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Details));
+            }
+            return View(account);
+        }*/
+        #endregion
 
         // GET: AdminController/ListAllAccounts
         [AdminAuthorizationFilter]
